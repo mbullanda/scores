c53bbade3b341c3f23df01a5c01d713e47657419
Uzytkownik <uzytkownik@kursGIT.pl>
2021-03-01 14:46:41 +0100

    done: model, dao, service for club, player, country and coach

--- src/main/java/dao/CountryDao.java
+++ src/main/java/dao/CountryDao.java
@@ -1,4 +1,28 @@
 package dao;
 
+import model.Country;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
 public class CountryDao {
+
+    public void saveCountry(Country country) {
+        EntityManagerFactory factory = SessionConnector.createFactory(Country.class);
+        EntityManager entityManager = factory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        transaction.begin();
+
+        entityManager.persist(country);
+
+        System.out.println("Saving country: " + country);
+
+        transaction.commit();
+        entityManager.close();
+    }
+
+    public boolean isCountryPresent(Country country) {
+        return false;
+    }
 }
--- src/main/java/service/CountryService.java
+++ src/main/java/service/CountryService.java
@@ -1,4 +1,19 @@
 package service;
 
+import dao.CountryDao;
+import lombok.AllArgsConstructor;
+import model.Country;
+
+@AllArgsConstructor
 public class CountryService {
+    private final CountryDao countryDao;
+
+    public void saveCountry (Country country){
+        if (countryDao.isCountryPresent(country)){
+            System.out.println("Country already present");
+        } else {
+            countryDao.saveCountry(country);
+        }
+    }
+
 }
